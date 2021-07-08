0984fdb6d906d5794224bf9fce1bd11618094935
Jorge Vargas <yorch.veintemil+github@gmail.com>
2021-07-08 13:09:39 -0700

    test up arrow moves up

--- tests/test_input_handlers.py
+++ tests/test_input_handlers.py
@@ -1,6 +1,7 @@
 import pytest
 import tcod.event
 
+from rogueliketcodtutorial2021.actions import MovementAction
 from rogueliketcodtutorial2021.input_handlers import EventHandler
 
 
@@ -19,3 +20,11 @@ def test_ev_keydown_returns_none_if_no_match() -> None:
     event = tcod.event.KeyDown(0, 0, 0)
 
     assert handler.ev_keydown(event) is None
+
+
+def test_ev_keydown_K_UP_moves_up() -> None:
+    handler = EventHandler()
+
+    event = tcod.event.KeyDown(0, tcod.event.K_UP, 0)
+
+    assert handler.ev_keydown(event) == MovementAction(dx=0, dy=-1)
