38a9e7313378b692ceb21a2d75e8391019761b65
Jorge Vargas <yorch.veintemil+github@gmail.com>
2021-07-08 13:35:49 -0700

    rename test and parameters to use as a generic action tester

--- tests/test_input_handlers.py
+++ tests/test_input_handlers.py
@@ -1,7 +1,7 @@
 import pytest
 import tcod.event
 
-from rogueliketcodtutorial2021.actions import MovementAction
+from rogueliketcodtutorial2021.actions import EscapeAction, MovementAction
 from rogueliketcodtutorial2021.input_handlers import EventHandler
 
 
@@ -15,18 +15,19 @@ def test_ev_quit_raises_system_exit() -> None:
 
 
 @pytest.mark.parametrize(
-    "sym, movement",
+    "sym, action",
     [
         (0, None),
         (tcod.event.K_UP, MovementAction(0, -1)),
         (tcod.event.K_DOWN, MovementAction(0, 1)),
         (tcod.event.K_LEFT, MovementAction(-1, 0)),
         (tcod.event.K_RIGHT, MovementAction(1, 0)),
+        (tcod.event.K_ESCAPE, EscapeAction()),
     ],
 )
-def test_ev_keydown_moves_correctly(sym, movement) -> None:
+def test_ev_keydown_performs_correct_action(sym, action) -> None:
     handler = EventHandler()
 
     event = tcod.event.KeyDown(0, sym, 0)
 
-    assert handler.ev_keydown(event) == movement
+    assert handler.ev_keydown(event) == action
