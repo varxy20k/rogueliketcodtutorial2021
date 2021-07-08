d60ae3a9a285666158ecda0515306ffbf5d99dc7
Jorge Vargas <yorch.veintemil+github@gmail.com>
2021-07-08 13:13:51 -0700

    test no key match does nothing

--- tests/test_input_handlers.py
+++ tests/test_input_handlers.py
@@ -11,3 +11,11 @@ def test_ev_quit_raises_system_exit() -> None:
 
     with pytest.raises(SystemExit):
         handler.ev_quit(event)
+
+
+def test_ev_keydown_returns_none_if_no_match() -> None:
+    handler = EventHandler()
+
+    event = tcod.event.KeyDown(0, 0, 0)
+
+    assert handler.ev_keydown(event) is None
