e16fc5afec3206c319b11602401f3195ee85a8ac
Jorge Vargas <yorch.veintemil+github@gmail.com>
2021-07-08 13:15:55 -0700

    pass test no key match does nothing

--- rogueliketcodtutorial2021/input_handlers.py
+++ rogueliketcodtutorial2021/input_handlers.py
@@ -8,3 +8,6 @@ from rogueliketcodtutorial2021.actions import Action
 class EventHandler(tcod.event.EventDispatch[Action]):
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        return None
